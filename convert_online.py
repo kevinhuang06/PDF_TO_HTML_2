b46ad7600e4de5cce26241cf8be73b76c9e4f9fa
kevinhuang06 <huangwenhan06@163.com>
2017-10-26 21:29:16 +0800

    stable online

--- convert_online.py
+++ convert_online.py
@@ -2,22 +2,35 @@
 
 import os
 import sys
+import signal
+
+
 from config import Config
 from storage import Oss
 from storage import Task
 from storage import AnnounceSql
 from simple_pdf2html import *
 
-task_name = 'test'
+switch_on = True 
+def sig_handler(sig, frame):
+    global switch_on
+    switch_on = False
+    print 'signal: kill'
+
+
+
+task_name = sys.argv[1]
 task = Task()
 oss = Oss(task_name)
 ann_sql = AnnounceSql()
 
-while True:
+signal.signal(signal.SIGTERM, sig_handler)
+while switch_on:
     oss_pdf = task.get_task()
     if oss_pdf is not None:
         # 下载 pdf
         pdf_storage_path = os.path.join(Config.PDF_STORAGE_DIR, oss_pdf)
+        print 'downloading :%s'% oss_pdf
         if oss.download(oss_pdf, pdf_storage_path):
             # 解析 pdf -> json
             with simplePDF2HTML(pdf_storage_path, task_name) as wizard: # 向魔法一样转换
@@ -28,6 +41,5 @@ while True:
                     oss.upload(wizard.json_path)
                     # 写 oss_json -> mysql
                     ann_sql.update_item(oss_pdf)
+                    print 'finish mysql update: %s'%oss_pdf
         #完成一个pdf 的转换
-    sys.exit(0)
-
